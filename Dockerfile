41e10cdd88b543812bfd7c91ef28d074bfc49e6b
Awodumila, Tobi Ebenezer <tobi@mail.com>
2023-11-01 18:21:59 +0100

    added docker

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:18
+
+RUN npm i -g pnpm
+
+WORKDIR usr/src/app
+
+COPY package.json pnpm-lock.yaml ./
+
+RUN pnpm install --frozen-lockfile
+
+COPY . .
+
+EXPOSE 3000
+
+CMD ["pnpm", "start"]
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello world");
+import express from "express"
+import morgan from "morgan"
+
+const app = express()
+
+app.use(morgan("dev"));
+
+app.get('/',(req,res)=>{
+    return res.json({
+        "message": "hello Tobi3",
+    });
+})
+
+const port = Number(process.env.PORT || 3000)
+app.listen(port,"0.0.0.0",()=>{
+    console.log("listening on port "+port)
+})
